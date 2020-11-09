32a0fbd9bab3e7ec47ff497e01988ba50ca02d15
nikldev0 <57417252+nikldev0@users.noreply.github.com>
2020-11-09 16:59:18 +0000

    First Commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Listen to new Shakira album",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Solve Rubik's cube",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Mow lawn",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Catch up on Mindhunter",
+        completed: false
+    }
+]
+
+export default todosData
